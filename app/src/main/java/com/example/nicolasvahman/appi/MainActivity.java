27c9d0a3c0675faa2126aa6c3e8f2e3a53d5a4c3
Nicolas Vahman <nicolasvahman@Nicolass-iMac.local>
2018-09-12 13:54:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nicolasvahman/appi/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.nicolasvahman.appi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.sql.BatchUpdateException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView3);
+        imageView1.setImageResource(R.drawable.greve);
+
+
+
+    }
+}
