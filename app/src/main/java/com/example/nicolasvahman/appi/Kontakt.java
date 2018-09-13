2a81931fba34c5fb765d1b47ef5affcacd9567b4
nicolasvahman <kavehv@live.dk>
2018-09-13 11:07:20 +0200

    Email fungerere med mulighed for at sende emails fra appen - fx kontakt trænere.

--- app/src/main/java/com/example/nicolasvahman/appi/Kontakt.java
+++ app/src/main/java/com/example/nicolasvahman/appi/Kontakt.java
@@ -6,21 +6,51 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Kontakt extends AppCompatActivity {
+
+
+    private EditText editTextto;
+    private EditText editTextEmne;
+    private EditText editTextMessage;
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.example_menu, menu);
         return true;
+
+
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kontakt);
+
+        editTextto = findViewById(R.id.to);
+        editTextEmne = findViewById(R.id.editText5);
+        editTextMessage = findViewById(R.id.editText6);
+
+
+        Button send = (findViewById(R.id.button_send));
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendMail();
+            }
+        });
+
+
     }
 
     @Override
@@ -71,4 +101,24 @@ public class Kontakt extends AppCompatActivity {
         intent4 = new Intent(this, Kontakt.class);
         startActivity(intent4);
     }
+
+    public void sendMail(){
+        String modtagere = editTextto.getText().toString();
+        String[] modtagerliste = modtagere.split(",");
+
+        String emne = editTextEmne.getText().toString();
+        String besked = editTextMessage.getText().toString();
+
+        Intent intent4 = new Intent(Intent.ACTION_SEND);
+        intent4.putExtra(Intent.EXTRA_EMAIL, modtagere);
+        intent4.putExtra(Intent.EXTRA_SUBJECT,emne);
+        intent4.putExtra(Intent.EXTRA_TEXT, besked);
+
+        intent4.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent4, "Choose and email client"));
+
+    }
+
+
+
 }
